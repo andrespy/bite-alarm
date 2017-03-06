94361d455b23c543ec05996d721a24ffb4015cf0
Unknown <andresprietoyanes@gmail.com>
2017-03-06 12:17:19 +0100

    EEPROM

--- code/bite_alarm/bite_alarm.ino
+++ code/bite_alarm/bite_alarm.ino
@@ -1,4 +1,4 @@
-#include <avr/pgmspace.h>
+#include <EEPROM.h>
 
 #define MAX_VOLUME 0
 #define MIN_VOLUME 0
@@ -11,14 +11,27 @@ const byte menuPin     = 0; // pin in which menu button will be connected
 
 bool menuStatus; // this bool stores the menu toggle status 1 stands for Volume and 0 for Sensitivity
 
-unsigned int volume;
-unsigned int sensitivity;
 
+struct parameter {
+  int value;
+  int address;
+}volume, sensitivity;
+
+void decrease();
+void increase(); 
+void toggleMenu();
 
 void setup() {
   pinMode(increasePin, INPUT_PULLUP);
   pinMode(decreasePin, INPUT_PULLUP);
   pinMode(menuPin, INPUT_PULLUP);
+
+  volume.address      = 0;
+  sensitivity.address = 1;
+
+  volume.value      = EEPROM.read(volume.address);
+  sensitivity.value = EEPROM.read(sensitivity.address);
+  
   attachInterrupt(digitalPinToInterrupt(increasePin), increase, FALLING);  //when an interruption occurs on falling edge (it's pulled up) system calls increase
   attachInterrupt(digitalPinToInterrupt(increasePin), decrease, FALLING);  //when an interruption occurs on falling edge (it's pulled up) system calls decrease
   attachInterrupt(digitalPinToInterrupt(increasePin), toggleMenu, FALLING);  //when an interruption occurs on falling edge (it's pulled up) system calls toggleMenu
@@ -30,23 +43,32 @@ void loop() {
 
 }
 
-void increase(){
+void increase()
+{
   if( menuStatus)
     {
-      if(volume<MAX_VOLUME) volume++;
+      if(volume.value < MAX_VOLUME) 
+      { volume.value++;
+        EEPROM.write(volume.address,volume.value);
+      }
     }
    else{
-      if(sensitivity<MAX_SENSITIVITY) sensitivity++;
+        if(sensitivity.value < MAX_SENSITIVITY) 
+          {
+            sensitivity.value++;
+            EEPROM.write(sensitivity.address,sensitivity.value);
+          }
+   
    }
 }
 
 void decrease(){
   if( menuStatus)
     {
-      if(volume>MIN_VOLUME) volume--;
+      if(volume.value > MIN_VOLUME) volume.value--;
     }
    else{
-      if(sensitivity>MIN_SENSITIVITY) sensitivity--;
+      if(sensitivity.value>MIN_SENSITIVITY) sensitivity.value--;
    }
 }
 
